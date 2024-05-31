730d3e8b799797428429f1fcc0f9ac5ebf0b24c2
Gauthier <e2290836@cmaisonneuve.qc.ca>
2024-05-31 13:45:53 -0400

    Création du dépot

---
+++ pays.php
@@ -0,0 +1,11 @@
+<?php
+   /**
+ * Package Pays
+ * Version 1.0.0
+ */
+/*
+Plugin name: Pays
+Plugin uri: https://github.com/s1v0lc/ef-pays
+Version: 1.0.0
+Description: Génère un menu constitué des noms des pays et selon le pays sélectionné affiche l’ensemble des destinations de ce pays.
+*/
